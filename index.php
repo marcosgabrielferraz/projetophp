cdcb9974d9f38ccfdef5d998c004d923192c6737
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 17:02:44 -0300

    Hello word

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<HTML>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+        <?php
+          ech("Hello Word")
+        ?>
+
+    </body>
+</HTML>
