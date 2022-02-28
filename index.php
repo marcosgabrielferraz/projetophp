f0ae3b619827a8d576434da75f235562601c71ac
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 18:14:55 -0300

    Varisbles

--- index.php
+++ index.php
@@ -7,7 +7,13 @@
     <body>
 
         <?php
-          ech("Hello Word")
+            $characterName = "Tom";
+            $characterAge = 80;
+            echo "There once was a man  named $characterName <br>";
+            echo "He was $characterage years old <br>";
+            echo "He really liked the name $characterName <br>";
+            echo "But didn't like being $characterAge <br>";
+
         ?>
 
     </body>
