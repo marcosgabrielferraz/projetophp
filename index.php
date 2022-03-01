fcd386828917b2846b84b9e27b644ed644d1ab74
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-03-01 10:00:30 -0300

    Constructors

--- index.php
+++ index.php
@@ -12,20 +12,19 @@
             var $author;
             var $pages;
 
+
+            function _construct($aTitle, $aAuthor, $aPages){
+                    $this->title = $aTitle;
+                    $this->author = $aAuthor;
+                    $this->pages = $aPages;
+            }
            }
 
-           $book1 = new Book;
-           $book1 -> title = "Harry Potter";
-           $book1 -> author = "JK Rowling";
-           $book1 -> pages = 400;
+           $book1 = new Book("Harry", "JK Rowling", 400);
+           $book1 -> title = "Hunger Games";
+           $book2 = new Book("Lord of the Rings", "Tolkien", 700);
 
-           $book2 = new Book;
-           $book2 -> title = "Lord of thi Rings";
-           $book2 -> author = "Tolkien";
-           $book2 -> pages = 700;
-
-           echo $book2 ->author;
-
+        echo $book1-> title;
         ?>
     
 
