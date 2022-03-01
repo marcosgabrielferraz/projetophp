40de409a68cb4b385c7031b2d3200bdfd9b4c8c2
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 21:31:14 -0300

    Associative Arrays

--- index.php
+++ index.php
@@ -5,17 +5,16 @@
         <title></title>
     </head>
     <body>
-        <foom action= "site.php" method= "post">
-            Apples<input type= "checkbox" name="fruits[]" value="apples"><br>
-            orangess<input type= "checkbox" name="fruits[]" value="oranges"><br>
-            Pears<input type= "checkbox" name="fruits[]" value="pears"><br>
-            <input type="submit">
-        </form>
 
-            <?php
-                $fruits = $_POST["fruits"];
-                echo $fruts [0];
-            ?>
+        <form action="site.php" mathod="post">
+            <input type="text" name="student">
+            <input type="submit">
+        </form> 
+
+        <?php
+           $grades = array("Jim"=>"A+", "pam"=>"A-","Oscar"=>"C-")
+           echo $grades[$_POST["student"]];
+        ?>
 
 
 
