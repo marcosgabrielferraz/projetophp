78469d1f044e0c09dc2d52cc46c5ca721cee692a
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-03-01 08:29:47 -0300

    While Loops

--- index.php
+++ index.php
@@ -6,33 +6,12 @@
     </head>
     <body>
 
-        <form action="site.php" method="post">
-            What was your grade?
-            <input type="text" name="grade">
-            <input type="submit">
-        </form>
-
         <?php
-        $grade = $_POST["grade"]
-        switch($grade){
-            case "A":
-                echo "You did amazing!";
-                break;
-            case "B":
-                echo "You did  pretty good";
-                break;
-            case "C":
-                echo "You did poorly";
-                break;
-            case "D"
-                echo "You did very bad";
-                break;
-            case "F":
-                echo "YOU FAIL!";
-                break;
-            default:
-                echo "Invalid Grade";
-        }
+            $index = 6,
+            do{  
+                echo "$index <br>";
+                $index++;
+            } while($index <= 5);
         ?>
 
 
