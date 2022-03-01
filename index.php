f3676e675a3999870b6509ef31abc42b82dcdc2a
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-03-01 09:47:06 -0300

    Classes & Objects

--- index.php
+++ index.php
@@ -7,9 +7,25 @@
     <body>
 
         <?php 
-            include "useful-tools.php";
-            echo #feeInMile;
-            
+           class Book{
+            var $title;
+            var $author;
+            var $pages;
+
+           }
+
+           $book1 = new Book;
+           $book1 -> title = "Harry Potter";
+           $book1 -> author = "JK Rowling";
+           $book1 -> pages = 400;
+
+           $book2 = new Book;
+           $book2 -> title = "Lord of thi Rings";
+           $book2 -> author = "Tolkien";
+           $book2 -> pages = 700;
+
+           echo $book2 ->author;
+
         ?>
     
 
